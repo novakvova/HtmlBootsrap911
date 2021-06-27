14827ef7a359292d1abaf1a131c27e059d3d54ca
novakvova <novakvova@gmail.com>
2021-06-27 09:59:09 +0300

    Begin js

---
+++ js/contacts.js
@@ -0,0 +1,10 @@
+﻿//коли DOM дерово готове
+
+window.onload = function () {
+    var tt = 23;
+    //var bb = "sdd" + tt;
+    //alert(bb);
+    document.getElementById("btnOk").onclick = function () {
+        console.log(tt);
+    };
+}
